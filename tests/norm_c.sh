eb9c055a782af6b6a95dff3bdfabedae10002fc5
Yealing Tay <yetay@student.42kl.edu.my>
2023-09-12 08:05:02 +0800

    Modified command to check norminette on files in subdirectory to use wildcard and mindepth 1, for aesthetic reasons

--- tests/norm_c.sh
+++ tests/norm_c.sh
@@ -1,8 +1,20 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    norm_c.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/09/12 08:03:52 by yetay             #+#    #+#              #
+#    Updated: 2023/09/12 08:04:01 by yetay            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/bash
 
 norminette -R CheckDefine *.h;
 norminette -R CheckForbiddenSourceHeader *c;
-find . -name "*.h" -mindepth 2 -exec norminette -R CheckDefine {} \; ;
-find . -name "*.c" -mindepth 2 \
+find * -name "*.h" -mindepth 1 -exec norminette -R CheckDefine {} \; ;
+find * -name "*.c" -mindepth 1 \
 	-exec norminette -R CheckForbiddenSourceHeader {} \; ;
 exit 0;
