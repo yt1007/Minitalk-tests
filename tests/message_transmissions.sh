2d1bba9ab5b666b86b141573a10398c66e73528b
Yealing Tay <yetay@student.42kl.edu.my>
2023-09-14 17:47:48 +0800

    For message transmission, only check if client can pass one message to server successfully

--- tests/message_transmissions.sh
+++ tests/message_transmissions.sh
@@ -6,7 +6,7 @@
 #    By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/09/12 13:07:26 by yetay             #+#    #+#              #
-#    Updated: 2023/09/14 16:48:24 by yetay            ###   ########.fr        #
+#    Updated: 2023/09/14 17:45:25 by yetay            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,29 +18,54 @@ EC=0;
 ## Message transmissions (Mandatory)
 if [[ ${M} -eq 1 ]];
 then
+
+	## Preparation
 	make fclean >/dev/null 2>&1;
 	make >/dev/null 2>&1;
-	(set +m; nohup ./server >serv_pid 2>/dev/null &);
-	ps x | grep -w "\.\/server" | awk '{print $1}' >serv_psid;
-	export server=$(grep -owf serv_psid serv_pid);
-	sort -R ${WD}/input/0.txt | head -n 5 > input.tmp;
-	bash ${WD}/tests/send_lines_from_file.sh input.tmp;
-	EC=$?;
-	(set +m; kill -TERM ${server}) >/dev/null 2>&1;
-	cat -e serv_pid | sed "s/\$$//" | sed "s/\^@$//" \
-		| grep -vw "$server" > output.tmp;
-	if [[ ${EC} -ne 0 || "$(diff input.tmp output.tmp)" ]];
+	if [[ !(-e server && -e client) ]];
 	then
-		echo -ne "Message not identical ";
-		echo;
-		echo "Input";
-		cat input.tmp;
-		echo;
-		echo "Output";
-		cat output.tmp;
+		echo -ne "Compilation error ";
 		exit 1;
 	fi;
-   	rm input.tmp output.tmp serv_psid serv_pid;
+
+	## It should be possible to pass a message of any size.
+	## Using a for loop, pass 5 messages of random lengths
+	## and verify that the transmission is successful.
+
+	for i in $(seq 1 5);
+	do
+		# Start server & get server PID
+		(set +m; nohup ./server >serv_pid 2>/dev/null &);
+		ps x | grep -w "\.\/server" | awk '{print $1}' >serv_psid;
+		export server=$(grep -owf serv_psid serv_pid);
+
+		# Send a message of random length
+		head -n $[$RANDOM * 100 / 32767] ${WD}/input/0.txt \
+			| tail -n 1 > input.tmp;
+		./client ${server} "$(cat input.tmp)";
+		EC=$?;
+
+		# Kill server
+		(set +m; kill -TERM ${server}) >/dev/null 2>&1;
+
+		# Compare server output with the client output
+		cat -e serv_pid | sed "s/\$$//" | sed "s/\^@$//" \
+			| grep -vw "$server" > output.tmp;
+		if [[ ${EC} -ne 0 || "$(diff input.tmp output.tmp)" ]];
+		then
+			echo -ne "Message not identical ";
+			echo;
+			echo "Input";
+			cat input.tmp;
+			echo;
+			echo "Output";
+			cat output.tmp;
+			exit 1;
+		fi;
+
+		# Clean-up
+		rm input.tmp output.tmp serv_psid serv_pid;
+	done;
 fi;
 
 ## Goodbye
